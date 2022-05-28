162c90e585d74868619a2b642927b8c4cc59d5c9
anrepo <andrey.polenok@gmail.com>
2022-05-28 15:25:27 +0300

    Добавление базовой структуры проекта

---
+++ run.bat
@@ -0,0 +1,1 @@
+cls && hugo server -w
---
+++ themes/template-theme/static/js/script.js
@@ -0,0 +1,27 @@
+// // Передача аргументов
+// function multiply(a, b, c) {
+//     return a * b * c;
+// }
+// console.log(multiply(1, 2, 3));
+
+
+// // Каррирование
+// function multicarr(a) {
+//     return (b) => {
+//         return (c) => {
+//             return a * b * c
+//         }
+//     }
+// }
+// console.log(multicarr(1)(2)(3));
+
+
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		autoScrolling:true,
+		scrollHorizontally: true
+	});
+
+	//methods
+	$.fn.fullpage.setAllowScrolling(false);
+});
