23e941e80cb659b42e8ad265096faa744c6edf3f
anrepo <andrey.polenok@gmail.com>
2022-06-30 11:33:29 +0300

    Задано начало для валидации формы на JavaScript

--- themes/template-theme/static/js/components/feedback-form.js
+++ themes/template-theme/static/js/components/feedback-form.js
@@ -1,17 +1,27 @@
 const submitBtn = document.querySelector('.form-submit');
+const form = document.querySelector('.feedback-form');
+
+let requiredElems;
 
 submitBtn.addEventListener("click", () => {
     let name = document.querySelector('input#name').value;
-    // console.log(name);
-
     let email = document.querySelector('input#email').value;
-    // console.log(email);
-
     let body = document.querySelector('textarea#body').value;
-    // console.log(body);
-
-    let subject = `Feedback from "anrepo.github.io" by ${name}`;
-    // console.log(subject);
-
-    window.open(`mailto:andrey.polenok@gmail.com?subject=${subject}&mail=${email}&body=${body}`);
+    let subject = `Feedback "anrepo.github.io" by ${name}, mail: ${email}`;
+    window.open(`mailto:andrey.polenok@gmail.com?subject=${subject}&from=${email}&body=${body}`);
 })
+
+function preparationForm() {
+    let formElems = form.querySelectorAll('input, textarea, select');
+    console.log(formElems);
+
+    for (let i = 0; i <= formElems.length; i++) {
+        if (formElems[i].required) requiredElems.push(formElems[i]);
+    }
+
+    // console.log(requiredElems);
+}
+
+// function validateForm() {
+    
+// } 
